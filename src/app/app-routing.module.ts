c54c98bfc3082e04f2f0d453f255d9c577930cfd
Elmehdi <eaitbrahim@gmail.com>
2021-03-03 16:32:21 -0500

    Add about component and implement router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { ArticleListComponent } from './article-list/article-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'articles', component: ArticleListComponent
+},
+{
+  path: 'about', component: AboutComponent
+},
+{
+  path: '', component: ArticleListComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
