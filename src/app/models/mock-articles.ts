51d77f7bdfce41ce58c9b9a09ad9a243c600cd2f
Elmehdi <eaitbrahim@gmail.com>
2021-03-03 18:31:44 -0500

    Display article's content

--- src/app/models/mock-articles.ts
+++ src/app/models/mock-articles.ts
@@ -4,7 +4,7 @@ export const ARTICLES: Article[] = [
   {
     id: 1,
     title: 'My first article',
-    content: '',
+    content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore, vero in nemo perferendis sed iure mollitia, quidem alias eveniet laborum laudantium impedit voluptatem ab architecto, quia corrupti debitis culpa consequuntur.',
     description: 'This is my first article! It\'s great! Please read it',
     key: 'my-first-article',
     date: new Date(),
@@ -13,7 +13,7 @@ export const ARTICLES: Article[] = [
   {
     id: 2,
     title: 'My second article',
-    content: '',
+    content: '<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur beatae blanditiis aliquid iusto cumque recusandae exercitationem tempora maxime labore dolore, mollitia eius quibusdam amet ab magnam magni. Eligendi, sunt in?</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur beatae blanditiis aliquid iusto cumque recusandae exercitationem tempora maxime labore dolore, mollitia eius quibusdam amet ab magnam magni. Eligendi, sunt in?</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur beatae blanditiis aliquid iusto cumque recusandae exercitationem tempora maxime labore dolore, mollitia eius quibusdam amet ab magnam magni. Eligendi, sunt in?</p>',
     description: 'Also a great article',
     key: 'the-second-article',
     date: new Date(),
